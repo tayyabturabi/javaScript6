3d4dbe4eb32012a52841822ecc95f517613cf3f1
Muhammad Tayyab <786.mtayyab@gmail.com>
2024-04-14 20:54:30 +0500

    check

---
+++ app.js
@@ -0,0 +1,88 @@
+//================== Assignment # 6 ================== //
+                //== CHAPTER # 21-25 ==//
+
+
+//Q # 1
+
+// var firstName = prompt("Enter your first name:");
+// var fNameCap = firstName.slice(0,1).toUpperCase();
+// var fNameSml = firstName.slice(1).toLowerCase();
+// var fNameFinal = fNameCap+fNameSml
+
+
+// var lastName = prompt("Enter your last name:");
+// var lNameameCap = lastName.slice(0,1).toUpperCase();
+// var lNameSml = lastName.slice(1).toLowerCase();
+// var lNameFinal = lNameameCap+lNameSml
+
+// var fullName = fNameFinal + " " + lNameFinal;
+
+// alert("Hello, " + fullName + "! Welcome.");
+
+//Q # 2
+
+// var phoneModel = prompt("What is your favorite mobile phone model?", "Samsung Galaxy S6 Edge Plus");
+// var inputLength = phoneModel.length;
+// document.write("My favorite mobile phone model is: " + phoneModel + "<br>");
+// document.write("Length of String is : "+ inputLength)
+
+//Q # 3
+
+// var str = "pakistani"
+// var check = str.indexOf("n");
+// document.write("String:" + str + "<br>");
+// document.write("Index No :" + check )
+
+
+//Q # 4
+
+// var str = "Hello World"
+// var check = str.lastIndexOf("l");
+// document.write("String:" + str + "<br>");
+// document.write("Last Index of 'l' :" + check )
+
+//Q # 5
+
+// var str = "pakistani"
+// var check = str.charAt(3);
+// document.write("String :" + str + "<br>");
+// document.write("Index No :" + check )
+
+
+//Q # 6
+// var firstName = prompt("Enter your first name:");
+// var fNameCap = firstName.slice(0,1).toUpperCase();
+// var fNameSml = firstName.slice(1).toLowerCase();
+// var fNameFinal = fNameCap.concat(fNameSml);
+
+
+// var lastName = prompt("Enter your last name:");
+// var lNameameCap = lastName.slice(0,1).toUpperCase();
+// var lNameSml = lastName.slice(1).toLowerCase();
+// var lNameFinal = lNameameCap.concat(lNameSml);
+
+// var fullName = fNameFinal.concat(" " + lNameFinal);
+
+// alert("Hello, " + fullName + "! Welcome.");
+
+
+
+//Q # 7
+
+// var city = "Hyderabad"
+// var rep = city.replace("Hyder","Islam")
+// document.write(rep);
+
+
+//Q # 8
+var message = ("Ali and Sami are best friends. They play cricket and football together");
+var str2 = "&"
+
+
+
+
+
+
+
+
+
